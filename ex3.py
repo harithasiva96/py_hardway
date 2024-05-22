32431c768adca00956386558f60ea23a4fdb1533
haritha <harithasiva007@gmail.com>
2024-05-22 14:43:43 +0530

    second commit

--- ex3.py
+++ ex3.py
@@ -1,12 +1,16 @@
 print("I will now count my chickens:")
-print("Hens", 25+30/6) 
-print("Roosters", 100-25*3%4)
+  #Text within the quotes print the text as it is.
+  #Numerical operations without quotes give the output value.
+print("Hens", 25 + 30 / 6) 
+  #The % modulus operation perform the division of two numbers and take its remainder as result.
+print("Roosters", 100 - 25 * 3 % 4)
 print("Now I will count the eggs:")
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print("It is true that 3 + 2 < 5 - 7?")
 print(3 + 2 < 5 - 7)
 print("What is 3 + 2?", 3 + 2)
 print("What is 5 - 7", 5 - 7)
+  # ">" Evaluate whether its greater, and give "true" as output if its correct.
 print("Is it greater?", 5 > -2 ) 
 print("Is it greater or equal?", 5 >= -2 ) 
 print("Is it less or equal?", 5 <= -2 ) 
