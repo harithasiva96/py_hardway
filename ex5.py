7f6f77d7e738e8f22eeda93091220d9deefc28e9
haritha <harithasiva007@gmail.com>
2024-05-22 16:12:10 +0530

    Format string -Format string assign values to strings inside the special character {}. -eg: variable = "world"      f"Hello {variable}".

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 75 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+ 
+  # "f" stands for format string that embed variables inside a string by using "{}".
+print(f"Lets talk about {my_name}.")
+print(f"He's {my_weight} pounds heavy.")
+  # The string inside special character "{my_height}" hold a value 75.
+print(f"He's {my_height} inches tall.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
