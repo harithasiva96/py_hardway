0c47add4a6ca4c9235a207d505cbf40bb7879525
haritha <harithasiva007@gmail.com>
2024-05-22 12:59:53 +0530

    A good first program -Print text within quotes. -Hash/Octothorpe is used to comment the lines so that it prevent the codes execution.

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I would much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# print("hello world")
