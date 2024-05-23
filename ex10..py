393b4d198d6242edae4f2c30ffd2f5f33caf9bc9
haritha <harithasiva007@gmail.com>
2024-05-23 10:31:53 +0530

    What was that? -Get into more ways of printing.  -Add tabs.  -Split lines.  -Add backslash.

---
+++ ex10..py
@@ -0,0 +1,18 @@
+  # \t gives a horizontal tab
+tabby_cat = "\tI'm tabbed in."
+  # To split a line we use backslash \
+persian_cat = "I am split\non a line."
+  # To add backslash
+backslash_cat = "I am \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\nt* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
