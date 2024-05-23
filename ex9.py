22776b609d2503cfc378ad18cdd38ca5137bdf3b
haritha <harithasiva007@gmail.com>
2024-05-23 10:19:50 +0530

    Printing, Printing, Printing - Explored different ways of printing.   -Print vertically.   -Print Horizontally.   -Print ultiple lines.

---
+++ ex9.py
@@ -0,0 +1,17 @@
+  # Print the string within the quotes as it is.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+  # Print the months one by one vertically. \n is used to print in a new line.
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+print("Here are the days:", days)
+print("Here are the months:", months)
+  # To type multiple lines we use triple quotes.
+print(
+    """
+    Twinkle twinkle little stars.
+    How I wonder what you are.
+    Up above the world so high.
+    Like a diamond in the sky.
+    We'll be able to type as much as we like.
+    Even 5 lines, or 6 or 7.
+    """
+)
