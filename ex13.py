4aaafb66029d5260ccdbc9bf1250d8047f1b59ea
haritha <harithasiva007@gmail.com>
2024-05-23 14:27:58 +0530

    Parameters, Unpacking, Variables - Assign values to variables. - Pass arguments to python scripts.

---
+++ ex13.py
@@ -0,0 +1,11 @@
+  # Add features to the script from python libraries.
+from sys import argv
+  # The variable holds the arguments that you pass to your python script.
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("The first variable is:", first)
+print("The second variable is:", second)
+print("The third variable is:", third)
+
+  # Run the program and pass three command line arguments.
