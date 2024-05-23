cdf10dda28e3588f2303c2b3459b2a41f0723c3b
haritha <harithasiva007@gmail.com>
2024-05-23 12:08:24 +0530

    Asking questions -Asking questions and receiving reply on the same line. -Pressing enter key directs to next question.

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end =' ')
+age = input() # Give the input on the same line.
+print("How tall are you?", end = ' ')
+height = input()
+print("How much do you weigh?", end = ' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
