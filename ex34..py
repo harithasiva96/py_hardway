30f222784d6b774f348da93b6a4ccb1e3fc6daf8
haritha <harithasiva007@gmail.com>
2024-06-03 20:42:59 +0530

    Accessing elements if lists -Accessing elements of a list with ordinal numbers and cardinal numbers

---
+++ ex34..py
@@ -0,0 +1,26 @@
+animals = ['bear', 'python3', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# First ,second, third etc.. indicate ordinal numbers, while 1, 2, 3 etc.. indicate cardinal numbers.
+# The animal at 1.
+print(animals[1])
+
+# The third animal.
+print(animals[2])
+
+# The first animal.
+print(animals[0])
+
+# The animal at 3.
+print(animals[3])
+
+# The fifth animal.
+print(animals[4])
+
+# The animal at 2.
+print(animals[2])
+
+# The sixth animal.
+print(animals[5])
+
+# The animal at 4.
+print(animals[4])
